83dc95dfe1ae00f2f1f4e9fa36a4f3ef887f33de
Morymirco <morykoulibaly996@gmail.com>
2024-04-09 07:16:50 +0000

    bon

--- src/Pages/NewBook/Livres.jsx
+++ src/Pages/NewBook/Livres.jsx
@@ -89,6 +89,42 @@ function Livres({ onligne }) {
           </div>
         </div>
       )}
+      <div class="max-w-full md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl mx-auto bg-white p-6 rounded-lg shadow-sm">
+        <div class="flex justify-end">
+          <nav class="flex space-x-2" aria-label="Pagination">
+            <a
+              href="#"
+              class="relative inline-flex items-center px-4 py-2 text-sm bg-[#127CE5] from-violet-300 to-indigo-300 border border-fuchsia-100 hover:border-violet-100 text-white font-semibold cursor-pointer leading-5 rounded-md transition duration-150 ease-in-out focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10"
+            >
+              Precedent
+            </a>
+            <a
+              href="#"
+              class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-fuchsia-100 hover:bg-fuchsia-200 cursor-pointer leading-5 rounded-md transition duration-150 ease-in-out focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10"
+            >
+              1
+            </a>
+            <a
+              href="#"
+              class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-fuchsia-100 hover:bg-fuchsia-200 cursor-pointer leading-5 rounded-md transition duration-150 ease-in-out focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10"
+            >
+              2
+            </a>
+            <a
+              href="#"
+              class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-fuchsia-100 hover:bg-fuchsia-200 cursor-pointer leading-5 rounded-md transition duration-150 ease-in-out focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10"
+            >
+              3
+            </a>
+            <a
+              href="#"
+              class="relative inline-flex items-center px-4 py-2 text-sm bg-blue-600 from-violet-300 to-indigo-300 border border-fuchsia-100 hover:border-violet-100 text-white font-semibold cursor-pointer leading-5 rounded-md transition duration-150 ease-in-out focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10"
+            >
+              Suivant
+            </a>
+          </nav>
+        </div>
+      </div>
       <Footer />
     </div>
   );
